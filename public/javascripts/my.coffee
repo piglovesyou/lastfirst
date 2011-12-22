a375e38b501ced792b1a42287a20147519c5b4a8
piglovesyou <thepiglovesyou@gmail.com>
2011-12-22 09:28:21 +0900

    nice date

--- public/javascripts/my.coffee
+++ public/javascripts/my.coffee
@@ -1,11 +1,19 @@
+###
+ LastFirstApp
+
+ Depands on:
+   socket.io.js
+   underscore.js
+   jquery-1.7.js
+###
+
 
 ###
  Utils.
- Depends on underscore.js
 ###
 # common in server and client
 
-window.getFirstLetter_ = (str) ->
+getFirstLetter_ = (str) ->
   count = 1
   len = str.length
   if len >= 2 and /^[ゃ-ょ|ー]$/.test(str[1])
@@ -18,7 +26,7 @@ window.getFirstLetter_ = (str) ->
     result.push str.slice(0,i+1)
   result
 
-window.getLastLetter_ = (str) ->
+getLastLetter_ = (str) ->
   count = 1
   len = str.length
   lastIndex = len - 1
@@ -74,6 +82,87 @@ _.mixin
     for key of keyValuePairs
       document.cookie = key + '=' + keyValuePairs[key]
 
+  # @param {Date|Number|String} date
+  # @param {?String=} lang default is "ja"
+  niceDate: ((date, lang) ->
+    SECOND = 1000
+    MINUTE = 60  * SECOND
+    HOUR =   60  * MINUTE
+    DAY =   24  * HOUR
+    WEEK =   7   * DAY
+    MONTH =  31  * DAY
+    YEAR =   365 * DAY
+
+    langs =
+      en:
+        AGO:    ' ago'
+        AFTER:  ' after'
+        SECOND: ' second'
+        MINUTE: ' minute'
+        HOUR:   ' hour'
+        DAY:    ' day'
+        WEEK:   ' week'
+        MONTH:  ' month'
+        YEAR:   ' year'
+        PLURAL: 's'
+      ja:
+        AGO:    '前'
+        AFTER:  '後'
+        SECOND: '秒'
+        MINUTE: '分'
+        HOUR:   '時間'
+        DAY:   '日'
+        WEEK:   '週'
+        MONTH:  '月'
+        YEAR:   '年'
+
+    dateFormat =
+      en:
+        HOUR_MINUTE: "%_:%_"
+        MONTH_DATE: "%_.%_"
+        YEAR_MONTH_DATE: "%_.%_.%_"
+      ja:
+        HOUR_MINUTE: "%_:%_"
+        MONTH_DATE: ["%_", langs.ja.MONTH, "%_", langs.ja.DAY].join('')
+        YEAR_MONTH_DATE: ["%_", langs.ja.YEAR, "%_", langs.ja.MONTH,
+                           "%_", langs.ja.DAY].join('')
+
+    pluralize = (num, str, lang) ->
+      num = Math.floor(num)
+      if lang.PLURAL and num > 1
+        num + str + lang.PLURAL
+      else
+        num + str
+
+    formatize = (format, values...) ->
+      for value in values
+        format = format.replace("%_", value)
+      format
+
+    return (date, lang_) ->
+      date = new Date(date) if _.isString(date) or _.isNumber(date)
+      if _.isDate(date)
+        now = new Date().getTime()
+        lang_ = if _.isString(lang_) and langs[lang_] then lang_ else "ja"
+        lang = langs[lang_]
+        fmt = dateFormat[lang_]
+        dist = now - date.getTime()
+        agoOrLater = if dist > 0 then lang.AGO else lang.AFTER
+        dist = Math.abs(dist)
+
+        if      dist < MINUTE
+          "#{pluralize(dist/SECOND, lang.SECOND, lang)}#{agoOrLater}"
+        else if dist < HOUR
+          "#{pluralize(dist/MINUTE, lang.MINUTE, lang)}#{agoOrLater}"
+        else if dist < DAY
+          "#{pluralize(dist/HOUR, lang.HOUR, lang)}#{agoOrLater}"
+        else if dist < DAY * 3
+          "#{pluralize(dist/DAY, lang.DAY, lang)}#{agoOrLater}"
+        else if dist < YEAR
+          "#{formatize(fmt.MONTH_DATE, date.getMonth()+1, date.getDate())}"
+        else
+          "#{formatize(fmt.YEAR_MONTH_DATE, date.getFullYear(), date.getMonth()+1, date.getDate())}"
+  )()
 
 
         
@@ -218,24 +307,26 @@ _.mixin
 ###
  Sockets init
 ###
-renderPastWord = (docs) ->
+renderWords = (docs) ->
   currentDocs_ = docs
   $list = $list_ or ($list_ = $('#word-list'))
   $list.empty()
   for doc in docs
     postfix = ''
+    niceDate = _.niceDate(doc.createdAt)
     if _.isEndsN(doc.content)
       postfix = '<span class="warn">*</span>'
     html = "<tr>"
     html += "<td>#{doc.content}#{postfix} </td>"
     html += "<td>&lt;-last post </td>" if _i is 0
-    html += "<td>&lt;-your post!</td>" if doc.createdBy is _.getUserId()
+    html += "<td>&lt;-your post! </td>" if doc.createdBy is _.getUserId()
+    html += "<td>#{niceDate}</td>" if _i < 2
     html += "</tr>"
     $list.append html
 
 socket = io.connect(location.protocol + '//' + location.host)
 socket.on 'update', (docs) ->
-  renderPastWord(docs)
+  renderWords(docs)
 
 socket.on 'need login', () ->
   _.showMessage('Expired. Need another login.')
--- public/javascripts/my.js
+++ public/javascripts/my.js
@@ -1,10 +1,18 @@
 (function() {
   /*
-   Utils.
-   Depends on underscore.js
+   LastFirstApp
+  
+   Depands on:
+     socket.io.js
+     underscore.js
+     jquery-1.7.js
   */
-  var $indicator_, $inputs_, $list_, $msgBox_, currentDocs_, delayTimerId_, postLocked_, renderPastWord, socket, userId_;
-  window.getFirstLetter_ = function(str) {
+  /*
+   Utils.
+  */
+  var $indicator_, $inputs_, $list_, $msgBox_, currentDocs_, delayTimerId_, getFirstLetter_, getLastLetter_, postLocked_, renderWords, socket, userId_;
+  var __slice = Array.prototype.slice;
+  getFirstLetter_ = function(str) {
     var count, len, result;
     count = 1;
     len = str.length;
@@ -21,7 +29,7 @@
     });
     return result;
   };
-  window.getLastLetter_ = function(str) {
+  getLastLetter_ = function(str) {
     var count, lastIndex, len;
     count = 1;
     len = str.length;
@@ -110,7 +118,99 @@
         _results.push(document.cookie = key + '=' + keyValuePairs[key]);
       }
       return _results;
-    }
+    },
+    niceDate: (function(date, lang) {
+      var DAY, HOUR, MINUTE, MONTH, SECOND, WEEK, YEAR, dateFormat, formatize, langs, pluralize;
+      SECOND = 1000;
+      MINUTE = 60 * SECOND;
+      HOUR = 60 * MINUTE;
+      DAY = 24 * HOUR;
+      WEEK = 7 * DAY;
+      MONTH = 31 * DAY;
+      YEAR = 365 * DAY;
+      langs = {
+        en: {
+          AGO: ' ago',
+          AFTER: ' after',
+          SECOND: ' second',
+          MINUTE: ' minute',
+          HOUR: ' hour',
+          DAY: ' day',
+          WEEK: ' week',
+          MONTH: ' month',
+          YEAR: ' year',
+          PLURAL: 's'
+        },
+        ja: {
+          AGO: '前',
+          AFTER: '後',
+          SECOND: '秒',
+          MINUTE: '分',
+          HOUR: '時間',
+          DAY: '日',
+          WEEK: '週',
+          MONTH: '月',
+          YEAR: '年'
+        }
+      };
+      dateFormat = {
+        en: {
+          HOUR_MINUTE: "%_:%_",
+          MONTH_DATE: "%_.%_",
+          YEAR_MONTH_DATE: "%_.%_.%_"
+        },
+        ja: {
+          HOUR_MINUTE: "%_:%_",
+          MONTH_DATE: ["%_", langs.ja.MONTH, "%_", langs.ja.DAY].join(''),
+          YEAR_MONTH_DATE: ["%_", langs.ja.YEAR, "%_", langs.ja.MONTH, "%_", langs.ja.DAY].join('')
+        }
+      };
+      pluralize = function(num, str, lang) {
+        num = Math.floor(num);
+        if (lang.PLURAL && num > 1) {
+          return num + str + lang.PLURAL;
+        } else {
+          return num + str;
+        }
+      };
+      formatize = function() {
+        var format, value, values, _i, _len;
+        format = arguments[0], values = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
+        for (_i = 0, _len = values.length; _i < _len; _i++) {
+          value = values[_i];
+          format = format.replace("%_", value);
+        }
+        return format;
+      };
+      return function(date, lang_) {
+        var agoOrLater, dist, fmt, now;
+        if (_.isString(date) || _.isNumber(date)) {
+          date = new Date(date);
+        }
+        if (_.isDate(date)) {
+          now = new Date().getTime();
+          lang_ = _.isString(lang_) && langs[lang_] ? lang_ : "ja";
+          lang = langs[lang_];
+          fmt = dateFormat[lang_];
+          dist = now - date.getTime();
+          agoOrLater = dist > 0 ? lang.AGO : lang.AFTER;
+          dist = Math.abs(dist);
+          if (dist < MINUTE) {
+            return "" + (pluralize(dist / SECOND, lang.SECOND, lang)) + agoOrLater;
+          } else if (dist < HOUR) {
+            return "" + (pluralize(dist / MINUTE, lang.MINUTE, lang)) + agoOrLater;
+          } else if (dist < DAY) {
+            return "" + (pluralize(dist / HOUR, lang.HOUR, lang)) + agoOrLater;
+          } else if (dist < DAY * 3) {
+            return "" + (pluralize(dist / DAY, lang.DAY, lang)) + agoOrLater;
+          } else if (dist < YEAR) {
+            return "" + (formatize(fmt.MONTH_DATE, date.getMonth() + 1, date.getDate()));
+          } else {
+            return "" + (formatize(fmt.YEAR_MONTH_DATE, date.getFullYear(), date.getMonth() + 1, date.getDate()));
+          }
+        }
+      };
+    })()
   });
   /*
    global accessor to the base info
@@ -275,8 +375,8 @@
   /*
    Sockets init
   */
-  renderPastWord = function(docs) {
-    var $list, doc, html, postfix, _i, _len, _results;
+  renderWords = function(docs) {
+    var $list, doc, html, niceDate, postfix, _i, _len, _results;
     currentDocs_ = docs;
     $list = $list_ || ($list_ = $('#word-list'));
     $list.empty();
@@ -284,6 +384,7 @@
     for (_i = 0, _len = docs.length; _i < _len; _i++) {
       doc = docs[_i];
       postfix = '';
+      niceDate = _.niceDate(doc.createdAt);
       if (_.isEndsN(doc.content)) {
         postfix = '<span class="warn">*</span>';
       }
@@ -293,7 +394,10 @@
         html += "<td>&lt;-last post </td>";
       }
       if (doc.createdBy === _.getUserId()) {
-        html += "<td>&lt;-your post!</td>";
+        html += "<td>&lt;-your post! </td>";
+      }
+      if (_i < 2) {
+        html += "<td>" + niceDate + "</td>";
       }
       html += "</tr>";
       _results.push($list.append(html));
@@ -302,7 +406,7 @@
   };
   socket = io.connect(location.protocol + '//' + location.host);
   socket.on('update', function(docs) {
-    return renderPastWord(docs);
+    return renderWords(docs);
   });
   socket.on('need login', function() {
     _.showMessage('Expired. Need another login.');
